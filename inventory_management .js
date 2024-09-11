2325d58a0ef8b176e36ed5effe951214a04d7598
Je4n-A <jeanma6742@gmail.com>
2024-09-11 15:58:46 -0400

    "Initialize inventory with product objects"

---
+++ inventory_management .js
@@ -0,0 +1,39 @@
+// Create an Inventory Array of Product Objects
+const inventory = [{
+    name: 'GPU',
+    price: 500.00,
+    quantity: 10,
+    lowStockLevel: 5
+
+},
+
+{
+    name: 'CPU',
+    price: 300.00,
+    quantity: 20,
+    lowStockLevel: 10
+
+},
+
+{
+    name: 'RAM',
+    price: 100.00,
+    quantity: 30,
+    lowStockLevel: 15
+},
+
+{
+    name: 'SSD',
+    price: 200.00,
+    quantity: 40,
+    lowStockLevel: 20
+},
+
+{
+    name: 'HDD',
+    price: 150.00,
+    quantity: 50,
+    lowStockLevel: 25
+}
+];
+
